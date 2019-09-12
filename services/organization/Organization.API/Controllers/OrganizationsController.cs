662ccdc54e2d4aad480e9ac90188c3a6060e98b8
panshao1006 <59138chp@sina.com>
2019-09-12 14:54:00 +0800

    组织服务

---
+++ services/organization/Organization.API/Controllers/OrganizationsController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Organization.Model;
+using Organization.Model.Filter;
+using Organization.Model.Model;
+
+namespace Organization.API.Controllers
+{
+    [Route("api/v1/organizations")]
+    public class OrganizationsController : Controller
+    {
+        [HttpGet]
+        public ReponseResult Get(OrganizationFilter filter)
+        {
+            ReponseResult result = new ReponseResult();
+
+            
+
+            return result;
+        }
+
+        [HttpPost]
+        public ReponseResult Post([FromBody]OrganizationModel user)
+        {
+            ReponseResult result = new ReponseResult();
+
+            return result;
+        }
+    }
+}
---
+++ services/organization/Organization.DAL/OrganizationRepository.cs
@@ -0,0 +1,19 @@
+﻿using Organization.Model.Filter;
+using Organization.Model.Model;
+using System;
+
+namespace Organization.DAL
+{
+    public class OrganizationRepository
+    {
+        public int CreateOrganization(OrganizationModel org)
+        {
+            return 1;
+        }
+
+        public OrganizationModel GetOrganization(OrganizationFilter filter)
+        {
+            return new OrganizationModel();
+        }
+    }
+}
