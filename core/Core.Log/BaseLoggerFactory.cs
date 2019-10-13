fd046852680a8dc18986de2140b11fb2520fe7b5
陈攀 <chp59138@hotmail.com>
2019-10-13 12:23:57 +0800

    增加文件

---
+++ core/Core.Log/BaseLoggerFactory.cs
@@ -0,0 +1,44 @@
+﻿using Core.Common;
+using Core.Http;
+using Core.Log.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Log
+{
+    public class BaseLoggerFactory
+    {
+        private string _logServiceHost = ConfigurationManager.AppSetting("LogFactoryHost");
+
+        private static BaseLoggerFactory _logFactory;
+
+        private BaseLoggerFactory()
+        {
+
+        }
+
+        public static BaseLoggerFactory Instance()
+        {
+            if(_logFactory == null)
+            {
+                _logFactory = new BaseLoggerFactory();
+            }
+
+            return _logFactory;
+        }
+
+        /// <summary>
+        /// 写入日志
+        /// </summary>
+        /// <param name="log"></param>
+        public void Write(LogDTO log)
+        {
+            HttpClientUtility httpClientUtility = new HttpClientUtility();
+
+            httpClientUtility.PostAsync<LogDTO>(_logServiceHost, log);
+        }
+
+
+    }
+}
---
+++ core/Core.Log/Model/LogDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Log.Model
+{
+    public class LogDTO
+    {
+        public string RequestId { set; get; }
+
+        public string AppName { set; get; }
+
+        public LogLevelType Level { set; get; }
+
+        public LogType Type { set; get; }
+
+        public object Content { set; get; }
+
+        public DateTime Date { get { return DateTime.UtcNow; } }
+    }
+}
